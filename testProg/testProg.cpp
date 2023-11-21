7f0904144ed86aeb46bb9faef3ac7f365d672c40
AndreKlim1 <andreyklimovich159@gmail.com>
2023-11-21 18:41:08 +0300

    Добавьте файлы проекта.

---
+++ testProg/testProg.cpp
@@ -0,0 +1,19 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <tlhelp32.h>
+
+int main()
+{
+    DWORD pid = GetCurrentProcessId();
+    //std::string testStr = "Hello";
+	char testStr[] = "Hello";
+	
+	std::cout << testStr << std::endl;
+	std::cout << pid << std::endl;
+	int a;
+	std::cin >> a;
+
+	std::cout << "Resutl: " << testStr << std::endl;
+	system("pause");
+}
